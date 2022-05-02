81952ba774815c3574be490850d2627eacfe8635
Joshua Loftin <joshua.loftin@aa.com>
2022-05-02 14:26:03 -0500

    add runClasses, printSeparator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import { run as runBasics } from './basics.js';
+import { run as runClasses } from './classes.js';
 
 runBasics();
+printSeparator();
+runClasses();
+printSeparator();
+
+function printSeparator () {
+    console.log('/n');
+    console.log('----------------------------------------------')
+    console.log('/n');
+}
