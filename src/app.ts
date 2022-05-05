282421ce9b0e8ac2a2b66d5e73fd97079e32a9ee
Joshua Loftin <joshua.loftin@aa.com>
2022-05-05 11:55:13 -0500

    Initial commit advanced

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 /// <reference path="./basics.ts" />
 /// <reference path="./classes.ts" />
 /// <reference path="./interfaces.ts" />
+/// <reference path="./advanced.ts" />
 
 runClasses();
 this.printSeparator();
 runInterfaces();
+this.printSeparator();
+runAdvanced();
 this.printSeparator();
 runBasics(); // Must run last due to intentional error
 this.printSeparator();
